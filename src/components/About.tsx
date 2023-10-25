06e5c053f5db01ab370fb2d5c4c34dbf8f502174
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-10-25 20:23:22 +0300

    add about content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,13 +4,13 @@ import Container from "./Container";
 const About: NextPage = () => {
 	return (
 		<>
-			<section className="about h-screen pt-14">
+			<section className="about h-full py-14">
 				<Container>
-					<h2 className="mb-6 text-4xl font-thin uppercase tracking-wide">
+					<h2 className="mb-5 text-4xl font-extralight uppercase tracking-wide">
 						Who
 						<span className="text-4xl font-medium"> we are</span>
 					</h2>
-					<p className="mb-6 text-sm font-thin tracking-wide leading-5">
+					<p className="mb-5 text-sm font-thin tracking-wide leading-5 w-[180px]">
 						<span className="font-medium">a team of enthusiasts </span>
 						who are fully committed to the mission of creating unforgettable and
 						extraordinary trips to the most beautiful parts of the Carpathians.
@@ -18,21 +18,23 @@ const About: NextPage = () => {
 						mountains, but to provide you with a deep immersion in their magical
 						atmosphere.
 					</p>
-					<p className="mb-6 text-sm font-thin tracking-wide leading-5">
+					<p className="mb-10 text-sm font-thin tracking-wide leading-5 w-[180px]">
 						<span className="font-medium">We believe </span>
 						that nature has the power to inspire, strengthen character and
 						provide new perspectives. Therefore, each of our tours is aimed at
 						unlocking your potential, enriching your spiritual world and
 						creating unforgettable memories.
 					</p>
-					<p className="whoweare-text-sub">
-						<span className="whoweare-text-up">From vacationers </span>
-						<span className="whoweare-text-up whoweare-text-right">
+					<p className="mb-10 w-[180px] ml-auto">
+						<span className="uppercase block">From vacationers </span>
+						<span className="uppercase text-right block">
 							to active travelers{" "}
 						</span>
-						we have a tour for everyone.
+						<span className="font-thin block tracking-[-0.14px]">
+							we have a tour for everyone.
+						</span>
 					</p>
-					<p className="mb-6 text-sm font-thin tracking-wide leading-5 auto-text">
+					<p className=" text-sm font-thin tracking-wide leading-5 auto-text">
 						<span className="font-medium">We use methods </span>
 						that are time-tested and proven. Our expert guides with in-depth
 						knowledge of the Carpathian landscapes lead you safely through the
